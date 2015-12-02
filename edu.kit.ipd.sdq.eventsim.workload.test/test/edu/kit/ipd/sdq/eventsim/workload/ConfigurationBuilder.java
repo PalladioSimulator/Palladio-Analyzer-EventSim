7dae7b9a0a65da37df4b1138506b5b8b4100a1b7
Philipp Merkle <merkle@kit.edu>
2015-12-02 13:24:49 +0000

    rename class
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@30104 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.workload.test/test/edu/kit/ipd/sdq/eventsim/workload/ConfigurationBuilder.java
+++ edu.kit.ipd.sdq.eventsim.workload.test/test/edu/kit/ipd/sdq/eventsim/workload/ConfigurationBuilder.java
@@ -7,33 +7,40 @@ import de.uka.ipd.sdq.simulation.AbstractSimulationConfig;
 import edu.kit.ipd.sdq.eventsim.api.PCMModel;
 import edu.kit.ipd.sdq.eventsim.middleware.simulation.config.SimulationConfiguration;
 
-public class SimulationConfigurationBuilder {
+public class ConfigurationBuilder {
 
-	private Map<String, Object> configMap;
+	private static final String UNLIMITED = "-1";
 
-	private PCMModel model;
+	private final Map<String, Object> configMap;
 
-	public SimulationConfigurationBuilder(PCMModel model) {
+	private final PCMModel model;
+
+	public ConfigurationBuilder(PCMModel model) {
 		configMap = new HashMap<>();
 		configMap.put(AbstractSimulationConfig.VERBOSE_LOGGING, false);
-		configMap.put(AbstractSimulationConfig.SIMULATION_TIME, "-1");
-		configMap.put(AbstractSimulationConfig.MAXIMUM_MEASUREMENT_COUNT, "-1");
+		configMap.put(AbstractSimulationConfig.SIMULATION_TIME, UNLIMITED);
+		configMap.put(AbstractSimulationConfig.MAXIMUM_MEASUREMENT_COUNT, UNLIMITED);
 		configMap.put(AbstractSimulationConfig.USE_FIXED_SEED, false);
 		configMap.put(AbstractSimulationConfig.PERSISTENCE_RECORDER_NAME, "Rserve Connector for EventSim");
+		configMap.put(AbstractSimulationConfig.EXPERIMENT_RUN, "Simulation test run"); // name of the experiment run
 		this.model = model;
 	}
 
-	public SimulationConfigurationBuilder stopAtSimulationTime(int time) {
+	public ConfigurationBuilder stopAtSimulationTime(int time) {
 		configMap.put(AbstractSimulationConfig.SIMULATION_TIME, new Integer(time).toString());
 		return this;
 	}
 
-	public SimulationConfigurationBuilder stopAtMeasurementCount(int count) {
+	public ConfigurationBuilder stopAtMeasurementCount(int count) {
 		configMap.put(AbstractSimulationConfig.MAXIMUM_MEASUREMENT_COUNT, new Integer(count).toString());
 		return this;
 	}
 
-	public SimulationConfiguration buildConfiguration() {
+	public SimulationConfiguration build() {
+		if (configMap.get(AbstractSimulationConfig.SIMULATION_TIME).equals(UNLIMITED)
+				&& configMap.get(AbstractSimulationConfig.MAXIMUM_MEASUREMENT_COUNT).equals(UNLIMITED)) {
+			throw new RuntimeException("Require at least one stopping criterion");
+		}
 		SimulationConfiguration config = new SimulationConfiguration(configMap, false);
 		config.setModel(model);
 		return config;

4387254e7da8cd52b653a9d85ea14ebec9c6adba
Philipp Merkle <merkle@kit.edu>
2015-12-03 14:55:26 +0000

    factor out model creation to separate class for reuse between test classes
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@30137 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.workload.test/test/edu/kit/ipd/sdq/eventsim/workload/DelayTests.java
+++ edu.kit.ipd.sdq.eventsim.workload.test/test/edu/kit/ipd/sdq/eventsim/workload/DelayTests.java
@@ -1,5 +1,7 @@
 package edu.kit.ipd.sdq.eventsim.workload;
 
+import static edu.kit.ipd.sdq.eventsim.workload.WorkloadModelHelper.createStartDelayStopActionChainInBehaviour;
+import static edu.kit.ipd.sdq.eventsim.workload.WorkloadModelHelper.createUsageScenarioWithClosedWorkloadInUsageModel;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -12,15 +14,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.MockitoAnnotations;
-import org.palladiosimulator.pcm.core.CoreFactory;
-import org.palladiosimulator.pcm.core.PCMRandomVariable;
-import org.palladiosimulator.pcm.usagemodel.ClosedWorkload;
 import org.palladiosimulator.pcm.usagemodel.Delay;
 import org.palladiosimulator.pcm.usagemodel.ScenarioBehaviour;
-import org.palladiosimulator.pcm.usagemodel.Start;
-import org.palladiosimulator.pcm.usagemodel.Stop;
 import org.palladiosimulator.pcm.usagemodel.UsageModel;
-import org.palladiosimulator.pcm.usagemodel.UsageScenario;
 import org.palladiosimulator.pcm.usagemodel.UsagemodelFactory;
 
 import com.google.inject.Guice;
@@ -129,41 +125,6 @@ public class DelayTests {
 		assertEquals(1.42, secondMeasurement.getWhen(), DELTA);
 
 		assertEquals(manager.getMiddleware().getSimulationControl().getCurrentSimulationTime(), 1.42, DELTA);
-	}
-
-	private Delay createStartDelayStopActionChainInBehaviour(ScenarioBehaviour b) {
-		Start start = UsagemodelFactory.eINSTANCE.createStart();
-		start.setScenarioBehaviour_AbstractUserAction(b);
-
-		Delay delay = UsagemodelFactory.eINSTANCE.createDelay();
-		delay.setScenarioBehaviour_AbstractUserAction(b);
-		delay.setPredecessor(start);
-
-		PCMRandomVariable delayTime = CoreFactory.eINSTANCE.createPCMRandomVariable();
-		delayTime.setSpecification("1.42");
-		delayTime.setDelay_TimeSpecification(delay);
-
-		Stop stop = UsagemodelFactory.eINSTANCE.createStop();
-		stop.setScenarioBehaviour_AbstractUserAction(b);
-		stop.setPredecessor(delay);
-		return delay;
-	}
-
-	private ScenarioBehaviour createUsageScenarioWithClosedWorkloadInUsageModel(UsageModel um, int population) {
-		UsageScenario s = UsagemodelFactory.eINSTANCE.createUsageScenario();
-		s.setUsageModel_UsageScenario(um);
-
-		ClosedWorkload w = UsagemodelFactory.eINSTANCE.createClosedWorkload();
-		w.setUsageScenario_Workload(s);
-		w.setPopulation(population);
-
-		PCMRandomVariable thinkTime = CoreFactory.eINSTANCE.createPCMRandomVariable();
-		thinkTime.setSpecification("0");
-		thinkTime.setClosedWorkload_PCMRandomVariable(w);
-
-		ScenarioBehaviour b = UsagemodelFactory.eINSTANCE.createScenarioBehaviour();
-		b.setUsageScenario_SenarioBehaviour(s);
-		return b;
 	}
 
 }

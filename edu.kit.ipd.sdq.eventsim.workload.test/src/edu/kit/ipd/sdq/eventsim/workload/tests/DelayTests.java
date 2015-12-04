cef2771c315497efaab92c37b23164459942881d
Philipp Merkle <merkle@kit.edu>
2015-12-04 10:50:57 +0000

    reorganize project structure
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@30151 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ edu.kit.ipd.sdq.eventsim.workload.test/src/edu/kit/ipd/sdq/eventsim/workload/tests/DelayTests.java
@@ -0,0 +1,137 @@
+package edu.kit.ipd.sdq.eventsim.workload.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.MockitoAnnotations;
+import org.palladiosimulator.pcm.usagemodel.Delay;
+import org.palladiosimulator.pcm.usagemodel.ScenarioBehaviour;
+import org.palladiosimulator.pcm.usagemodel.UsageModel;
+import org.palladiosimulator.pcm.usagemodel.UsageScenario;
+import org.palladiosimulator.pcm.usagemodel.UsagemodelFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import edu.kit.ipd.sdq.eventsim.api.PCMModel;
+import edu.kit.ipd.sdq.eventsim.launch.SimulationManager;
+import edu.kit.ipd.sdq.eventsim.measurement.Measurement;
+import edu.kit.ipd.sdq.eventsim.measurement.MeasurementFacade;
+import edu.kit.ipd.sdq.eventsim.measurement.MeasurementStorage;
+import edu.kit.ipd.sdq.eventsim.measurement.Metric;
+import edu.kit.ipd.sdq.eventsim.middleware.simulation.config.SimulationConfiguration;
+import edu.kit.ipd.sdq.eventsim.workload.EventSimWorkloadModel;
+import edu.kit.ipd.sdq.eventsim.workload.calculators.TimeSpanBetweenUserActionsCalculator;
+
+/**
+ * Tests simulation of {@link Delay} actions.
+ * 
+ * @author Philipp Merkle
+ *
+ */
+public class DelayTests {
+
+	private static final Level LOG_LEVEL = Level.DEBUG;
+
+	private static final double DELTA = 1e-10;
+
+	@SuppressWarnings("rawtypes")
+	@Captor
+	private ArgumentCaptor<Measurement> measurementArgument;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		Logger.getLogger("edu.kit.ipd.sdq.eventsim").setLevel(LOG_LEVEL);
+	}
+
+	@Test
+	public void delaysDontCauseContention_oneUsageScenario_twoConcurrentUsers() {
+		// create PCM usage model
+		UsageModel um = UsagemodelFactory.eINSTANCE.createUsageModel();
+		UsageScenario s = new UsageScenarioBuilder().closedWorkload(2, 0).buildIn(um);
+		ScenarioBehaviour b = new ScenarioBehaviourBuilder().start().delay("delay", 1.42).stop().buildIn(s);
+		PCMModel model = new PCMModelBuilder().withUsageModel(um).build();
+
+		// create simulation configuration
+		SimulationConfiguration config = new ConfigurationBuilder(model).stopAtMeasurementCount(2).build();
+
+		// assemble simulation components (some of them being mocked)
+		Injector injector = Guice.createInjector(new TestSimulationModule(config));
+		SimulationManager manager = injector.getInstance(SimulationManager.class);
+
+		// set up custom measuring points
+		Delay delay = (Delay) ScenarioBehaviourBuilder.actionByName(b, "delay");
+		MeasurementFacade<?> measurementFacade = ((EventSimWorkloadModel) manager.getWorkload()).getMeasurementFacade();
+		MeasurementStorage measurementStorage = mock(MeasurementStorage.class);
+		measurementFacade.createCalculator(new TimeSpanBetweenUserActionsCalculator(Metric.TIME_SPAN))
+				.from(delay, "before").to(delay, "after").forEachMeasurement(m -> measurementStorage.putPair(m));
+
+		// run simulation
+		manager.startSimulation();
+
+		// check simulation results
+		verify(measurementStorage, times(2)).putPair(measurementArgument.capture());
+		Measurement<?, ?> firstMeasurement = measurementArgument.getAllValues().get(0);
+		assertEquals(1.42, firstMeasurement.getValue(), DELTA);
+		assertEquals(1.42, firstMeasurement.getWhen(), DELTA);
+
+		Measurement<?, ?> secondMeasurement = measurementArgument.getAllValues().get(1);
+		assertEquals(1.42, secondMeasurement.getValue(), DELTA);
+		assertEquals(1.42, secondMeasurement.getWhen(), DELTA);
+
+		assertEquals(manager.getMiddleware().getSimulationControl().getCurrentSimulationTime(), 1.42, DELTA);
+	}
+
+	@Test
+	public void delaysDontCauseContention_twoUsageScenarios_oneUserPerScenario() {
+		// create PCM usage model
+		UsageModel um = UsagemodelFactory.eINSTANCE.createUsageModel();
+		UsageScenario s1 = new UsageScenarioBuilder().closedWorkload(1, 0).buildIn(um);
+		ScenarioBehaviour b1 = new ScenarioBehaviourBuilder().start().delay("delay1", 1.42).stop().buildIn(s1);
+		UsageScenario s2 = new UsageScenarioBuilder().closedWorkload(1, 0).buildIn(um);
+		ScenarioBehaviour b2 = new ScenarioBehaviourBuilder().start().delay("delay2", 1.42).stop().buildIn(s2);
+		PCMModel model = new PCMModelBuilder().withUsageModel(um).build();
+
+		// create simulation configuration
+		SimulationConfiguration config = new ConfigurationBuilder(model).stopAtMeasurementCount(2).build();
+
+		// assemble simulation components (some of them being mocked)
+		Injector injector = Guice.createInjector(new TestSimulationModule(config));
+		SimulationManager manager = injector.getInstance(SimulationManager.class);
+
+		// set up custom measuring points
+		Delay delay1 = (Delay) ScenarioBehaviourBuilder.actionByName(b1, "delay1");
+		Delay delay2 = (Delay) ScenarioBehaviourBuilder.actionByName(b2, "delay2");
+		MeasurementFacade<?> measurementFacade = ((EventSimWorkloadModel) manager.getWorkload()).getMeasurementFacade();
+		MeasurementStorage measurementStorage = mock(MeasurementStorage.class);
+		measurementFacade.createCalculator(new TimeSpanBetweenUserActionsCalculator(Metric.TIME_SPAN))
+				.from(delay1, "before").to(delay1, "after").forEachMeasurement(m -> measurementStorage.putPair(m));
+		measurementFacade.createCalculator(new TimeSpanBetweenUserActionsCalculator(Metric.TIME_SPAN))
+				.from(delay2, "before").to(delay2, "after").forEachMeasurement(m -> measurementStorage.putPair(m));
+
+		// run simulation
+		manager.startSimulation();
+
+		// check simulation results
+		verify(measurementStorage, times(2)).putPair(measurementArgument.capture());
+		Measurement<?, ?> firstMeasurement = measurementArgument.getAllValues().get(0);
+		assertEquals(1.42, firstMeasurement.getValue(), DELTA);
+		assertEquals(1.42, firstMeasurement.getWhen(), DELTA);
+
+		Measurement<?, ?> secondMeasurement = measurementArgument.getAllValues().get(1);
+		assertEquals(1.42, secondMeasurement.getValue(), DELTA);
+		assertEquals(1.42, secondMeasurement.getWhen(), DELTA);
+
+		assertEquals(manager.getMiddleware().getSimulationControl().getCurrentSimulationTime(), 1.42, DELTA);
+	}
+
+}
---
+++ edu.kit.ipd.sdq.eventsim.workload.test/src/edu/kit/ipd/sdq/eventsim/workload/tests/ScenarioBehaviourBuilder.java
@@ -0,0 +1,123 @@
+package edu.kit.ipd.sdq.eventsim.workload.tests;
+
+import java.util.UUID;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.palladiosimulator.pcm.core.CoreFactory;
+import org.palladiosimulator.pcm.core.PCMRandomVariable;
+import org.palladiosimulator.pcm.usagemodel.AbstractUserAction;
+import org.palladiosimulator.pcm.usagemodel.Branch;
+import org.palladiosimulator.pcm.usagemodel.BranchTransition;
+import org.palladiosimulator.pcm.usagemodel.Delay;
+import org.palladiosimulator.pcm.usagemodel.ScenarioBehaviour;
+import org.palladiosimulator.pcm.usagemodel.Start;
+import org.palladiosimulator.pcm.usagemodel.Stop;
+import org.palladiosimulator.pcm.usagemodel.UsageScenario;
+import org.palladiosimulator.pcm.usagemodel.UsagemodelFactory;
+
+public class ScenarioBehaviourBuilder {
+
+	private final ScenarioBehaviour behaviour;
+
+	private AbstractUserAction lastAction;
+
+	public ScenarioBehaviourBuilder() {
+		behaviour = UsagemodelFactory.eINSTANCE.createScenarioBehaviour();
+	}
+
+	public ScenarioBehaviourBuilder start(String name) {
+		Start start = UsagemodelFactory.eINSTANCE.createStart();
+		start.setEntityName(name);
+		enchain(start);
+		return this;
+	}
+
+	public ScenarioBehaviourBuilder start() {
+		return start(randomName());
+	}
+
+	public ScenarioBehaviourBuilder stop(String name) {
+		Stop stop = UsagemodelFactory.eINSTANCE.createStop();
+		stop.setEntityName(name);
+		enchain(stop);
+		return this;
+	}
+
+	public ScenarioBehaviourBuilder stop() {
+		return stop(randomName());
+	}
+
+	public ScenarioBehaviourBuilder delay(String name, double time) {
+		Delay delay = UsagemodelFactory.eINSTANCE.createDelay();
+		delay.setEntityName(name);
+		PCMRandomVariable delayTime = CoreFactory.eINSTANCE.createPCMRandomVariable();
+		delayTime.setSpecification(new Double(time).toString());
+		delayTime.setDelay_TimeSpecification(delay);
+		enchain(delay);
+		return this;
+	}
+
+	public ScenarioBehaviourBuilder delay(double time) {
+		return delay(randomName(), time);
+	}
+
+	public ScenarioBehaviourBuilder branch(String name, BranchTransition... transitions) {
+		Branch branch = UsagemodelFactory.eINSTANCE.createBranch();
+		branch.setEntityName(name);
+		for (BranchTransition t : transitions) {
+			t.setBranch_BranchTransition(branch);
+		}
+		enchain(branch);
+		return this;
+	}
+
+	public ScenarioBehaviourBuilder branch(BranchTransition... transitions) {
+		return branch(randomName(), transitions);
+	}
+
+	public static BranchTransition transition(double probability, ScenarioBehaviour behaviour) {
+		BranchTransition t = UsagemodelFactory.eINSTANCE.createBranchTransition();
+		t.setBranchProbability(probability);
+		t.setBranchedBehaviour_BranchTransition(behaviour);
+		return t;
+	}
+
+	public ScenarioBehaviour build() {
+		return behaviour;
+	}
+
+	public ScenarioBehaviour buildIn(UsageScenario scenario) {
+		behaviour.setUsageScenario_SenarioBehaviour(scenario);
+		return behaviour;
+	}
+
+	private void enchain(AbstractUserAction action) {
+		action.setScenarioBehaviour_AbstractUserAction(behaviour);
+		action.setPredecessor(lastAction);
+		lastAction = action;
+	}
+
+	private String randomName() {
+		return UUID.randomUUID().toString();
+	}
+
+	public static AbstractUserAction actionByName(BranchTransition t, String actionName) {
+		return actionByName(t.getBranchedBehaviour_BranchTransition(), actionName);
+	}
+	
+	public static AbstractUserAction actionByName(ScenarioBehaviour b, String actionName) {
+		TreeIterator<EObject> it = b.eAllContents();
+		while (it.hasNext()) {
+			EObject o = it.next();
+			if (o instanceof AbstractUserAction) {
+				AbstractUserAction a = (AbstractUserAction) o;
+				if (actionName.equals(a.getEntityName())) {
+					return a;
+				}
+			}
+		}
+		throw new RuntimeException("Could not find action named " + actionName);
+	}
+
+}

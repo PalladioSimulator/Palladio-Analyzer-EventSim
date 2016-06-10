8b8e5a9f8227683e9f36ce7b3fd493aa58a0ce4a
Philipp Merkle <merkle@kit.edu>
2016-06-10 17:56:25 +0000

    fix bug
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@32251 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.rvisualization/src/edu/kit/ipd/sdq/eventsim/rvisualization/handlers/ReloadHandler.java
+++ edu.kit.ipd.sdq.eventsim.rvisualization/src/edu/kit/ipd/sdq/eventsim/rvisualization/handlers/ReloadHandler.java
@@ -3,7 +3,8 @@ package edu.kit.ipd.sdq.eventsim.rvisualization.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 
 import edu.kit.ipd.sdq.eventsim.rvisualization.views.FilterView;
 
@@ -11,10 +12,15 @@ public class ReloadHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		FilterView view = (FilterView) HandlerUtil
-				.getActiveWorkbenchWindow(event).getActivePage()
-				.getActivePart();
-		
+		FilterView view;
+		try {
+			view = (FilterView) PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage()
+					.showView(FilterView.ID);
+		} catch (PartInitException e) {
+			throw new ExecutionException("Could not reload measurements", e);
+		}
+
 		view.getController().populateControls();
 
 		return null;

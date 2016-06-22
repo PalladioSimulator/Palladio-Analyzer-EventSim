28857ed84c584ca3f9e5d16e57e12c31e6f6a0c4
Philipp Merkle <merkle@kit.edu>
2016-06-22 14:01:46 +0000

    fix bug
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@32291 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.rvisualization/src/edu/kit/ipd/sdq/eventsim/rvisualization/RController.java
+++ edu.kit.ipd.sdq.eventsim.rvisualization/src/edu/kit/ipd/sdq/eventsim/rvisualization/RController.java
@@ -393,7 +393,7 @@ public final class RController {
         try {
             String rCmd = "tables(silent=TRUE)[NAME=='" + CONTENT_VARIABLE + "']$MB";
             REXP evaluated = evalRCommand(rCmd);
-            memoryConsumption = Integer.parseInt(evaluated.asString());
+            memoryConsumption = Integer.parseInt(evaluated.asString().replace(",", "").replace(".", ""));
         } catch (REXPMismatchException e) {
             LOG.error("Could not calculate memory consumption", e);
         } catch (EvaluationException e) {

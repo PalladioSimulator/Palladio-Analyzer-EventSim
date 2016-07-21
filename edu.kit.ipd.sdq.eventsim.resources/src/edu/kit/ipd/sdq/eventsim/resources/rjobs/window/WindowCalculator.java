a4fb59703067eca2ab8cea045ab8ee7feb0f3886
Philipp Merkle <merkle@kit.edu>
2016-07-21 15:38:38 +0000

    add javadoc
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@32571 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/rjobs/window/WindowCalculator.java
+++ edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/rjobs/window/WindowCalculator.java
@@ -2,6 +2,24 @@ package edu.kit.ipd.sdq.eventsim.resources.rjobs.window;
 
 public interface WindowCalculator {
 
-    double processValue(double value, double duration);
+    /**
+     * Calculates how the current window's value has to change when integrating the given
+     * observation:
+     * 
+     * <pre>
+     * currentWindow = currentWindow + processValue(observation, duration)
+     * </pre>
+     * 
+     * This method will be called at least once per window. The common case is, however, that this
+     * method is called multiple times for each window in order to incrementally fill the window.
+     * 
+     * @param observation
+     *            the observation to be integrated
+     * @param duration
+     *            the time span the observation is valid
+     * @return the summand that needs to be added (mathematically) to the current window's value so
+     *         that the observations subsumed by the window include the passed observation.
+     */
+    double processValue(double observation, double duration);
 
 }

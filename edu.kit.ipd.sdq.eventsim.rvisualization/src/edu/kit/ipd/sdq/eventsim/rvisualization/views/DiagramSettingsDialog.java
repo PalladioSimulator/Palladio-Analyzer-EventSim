d96cfe15221c5c14a3bdb7f804f84fb2120806dd
Philipp Merkle <merkle@kit.edu>
2016-08-04 13:29:50 +0000

    refine diagram settings dialog
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@32797 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.rvisualization/src/edu/kit/ipd/sdq/eventsim/rvisualization/views/DiagramSettingsDialog.java
+++ edu.kit.ipd.sdq.eventsim.rvisualization/src/edu/kit/ipd/sdq/eventsim/rvisualization/views/DiagramSettingsDialog.java
@@ -9,28 +9,39 @@ import org.eclipse.swt.widgets.Shell;
 import edu.kit.ipd.sdq.eventsim.rvisualization.model.DiagramModel;
 
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 
-public class DiagramSettingsDialog extends TitleAreaDialog  {
+public class DiagramSettingsDialog extends TitleAreaDialog {
 
     private DiagramModel diagramModel;
-    
+
     private DiagramSettingsViewer viewer;
-    
+
     public DiagramSettingsDialog(Shell parentShell, DiagramModel diagramModel) {
         super(parentShell);
+        setShellStyle(getShellStyle() | SWT.RESIZE | SWT.MAX);
         this.diagramModel = diagramModel;
     }
 
     @Override
     protected Control createDialogArea(Composite parent) {
-        Composite contents = new Composite(parent, SWT.NONE);
+        Composite area = (Composite) super.createDialogArea(parent);
+        Composite contents = new Composite(area, SWT.NONE);
+        contents.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
         contents.setLayout(new FillLayout(SWT.HORIZONTAL));
-        
+
+        setHelpAvailable(false);
         setTitle("Diagram Settings");
-        
+
         viewer = new DiagramSettingsViewer(contents, SWT.NONE, diagramModel);
-        
-        return contents;
+
+        return area;
+    }
+
+    @Override
+    protected void configureShell(Shell shell) {
+        super.configureShell(shell);
+        shell.setText("Diagram Settings");
     }
 
     @Override
@@ -38,8 +49,5 @@ public class DiagramSettingsDialog extends TitleAreaDialog  {
         viewer.applyChanges();
         super.okPressed();
     }
-    
-    
-    
-    
+
 }

04f096c236e06c392be637b949bd0457aa80b350
Philipp Merkle <merkle@kit.edu>
2016-11-23 15:14:56 +0000

    add documentation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@33861 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/launch/ResourcesModuleLaunchContribution.java
+++ edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/launch/ResourcesModuleLaunchContribution.java
@@ -11,6 +11,13 @@ import org.eclipse.swt.widgets.Group;
 
 import edu.kit.ipd.sdq.eventsim.modules.AbstractLaunchContribution;
 
+/**
+ * TODO the entire class is only a skeleton so far. Values supplied via GUI controls won't be
+ * applied to the configuration.
+ * 
+ * @author Philipp Merkle
+ *
+ */
 public class ResourcesModuleLaunchContribution extends AbstractLaunchContribution {
 
     /**
@@ -21,20 +28,20 @@ public class ResourcesModuleLaunchContribution extends AbstractLaunchContributio
         final Composite container = new Composite(parent, SWT.NONE);
         this.setControl(container);
         container.setLayout(new GridLayout());
-        
+
         Group grpMeasurements = new Group(container, SWT.NONE);
         grpMeasurements.setLayout(new GridLayout(1, false));
         grpMeasurements.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
         grpMeasurements.setText("Measurement Options");
-        
+
         Button btnCalcResourceUtilization = new Button(grpMeasurements, SWT.CHECK);
         btnCalcResourceUtilization.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
         btnCalcResourceUtilization.setText("Calculate Resource Utilization Metric");
-        
+
         Button btnCalcMeanQueueLength = new Button(grpMeasurements, SWT.CHECK);
         btnCalcMeanQueueLength.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
         btnCalcMeanQueueLength.setText("Calculate Mean Queue Length Metric");
-        
+
         Button btnDeleteRawQueueLength = new Button(grpMeasurements, SWT.CHECK);
         btnDeleteRawQueueLength.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
         btnDeleteRawQueueLength.setText("Delete Raw Queue Length Observations");

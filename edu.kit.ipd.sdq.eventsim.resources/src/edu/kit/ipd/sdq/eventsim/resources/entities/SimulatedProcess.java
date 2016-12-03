1c105ddf784d4e03a0c8c2c4e4a05f1d8a842ac2
Philipp Merkle <merkle@kit.edu>
2016-12-03 13:31:06 +0000

    fix considerable performance regression
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/EventSim/trunk@33879 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/entities/SimulatedProcess.java
+++ edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/entities/SimulatedProcess.java
@@ -1,6 +1,7 @@
 package edu.kit.ipd.sdq.eventsim.resources.entities;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.apache.log4j.Logger;
@@ -9,14 +10,15 @@ import org.palladiosimulator.pcm.seff.AbstractAction;
 import de.uka.ipd.sdq.scheduler.IActiveResource;
 import de.uka.ipd.sdq.scheduler.ISchedulableProcess;
 import de.uka.ipd.sdq.simulation.abstractsimengine.ISimulationModel;
-import edu.kit.ipd.sdq.eventsim.api.Procedure;
 import edu.kit.ipd.sdq.eventsim.api.IRequest;
+import edu.kit.ipd.sdq.eventsim.api.Procedure;
 import edu.kit.ipd.sdq.eventsim.entities.EventSimEntity;
 
 /**
- * A simulated process is a process that can be scheduled on an active or passive resource. Whenever
- * the scheduler activates or passivates the process, the {@link IProcessListener}, which has been
- * passed to the constructor, gets notified.
+ * A simulated process is a process that can be scheduled on an active or
+ * passive resource. Whenever the scheduler activates or passivates the process,
+ * the {@link IProcessListener}, which has been passed to the constructor, gets
+ * notified.
  * 
  * @author Philipp Merkle
  * 
@@ -24,184 +26,192 @@ import edu.kit.ipd.sdq.eventsim.entities.EventSimEntity;
  */
 public class SimulatedProcess extends EventSimEntity implements ISchedulableProcess {
 
-    private static final Logger logger = Logger.getLogger(SimulatedProcess.class);
+	private static final Logger logger = Logger.getLogger(SimulatedProcess.class);
 
-    private final ArrayList<IActiveResource> terminatedObservers;
-    private final IRequest request;
-    private boolean terminated;
-    private int priority;
-    private SimulatedProcess parent;
-    private Procedure onActivationCallback;
+	private final List<IActiveResource> terminatedObservers;
+	private final IRequest request;
+	private boolean terminated;
+	private int priority;
+	private SimulatedProcess parent;
+	private Procedure onActivationCallback;
 
-    public SimulatedProcess(ISimulationModel model, SimulatedProcess parent, final IRequest request) {
-        super(model, "SimulatedProcess");
-        this.parent = parent;
-        this.request = request;
-        this.terminatedObservers = new ArrayList<IActiveResource>();
-    }
+	private String id;
+	private int hashCode;
 
-    public void setOnActivationCallback(Procedure callback) {
-        if (onActivationCallback != null) {
-            logger.warn(String.format("Overriding existing activation callback for %s", this));
-        }
-        this.onActivationCallback = Objects.requireNonNull(callback);
-    }
+	public SimulatedProcess(ISimulationModel model, SimulatedProcess parent, final IRequest request) {
+		super(model, "SimulatedProcess");
+		this.parent = parent;
+		this.request = request;
+		this.terminatedObservers = new ArrayList<IActiveResource>();
+		this.id = Long.toString(getEntityId());
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void activate() {
-        if (onActivationCallback != null) {
-            // first reset this object's callback, then call it
-            Procedure callback = onActivationCallback;
-            onActivationCallback = null;
-            callback.execute();
-        } else {
-            logger.warn(String.format("Activating %s, but there is no activation callback registered", this));
-        }
-    }
+	public void setOnActivationCallback(Procedure callback) {
+		if (onActivationCallback != null) {
+			logger.warn(String.format("Overriding existing activation callback for %s", this));
+		}
+		this.onActivationCallback = Objects.requireNonNull(callback);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void passivate() {
-        if (onActivationCallback == null) {
-            logger.warn(String.format("Passivating %s, but there is no activation callback registered", this));
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void activate() {
+		if (onActivationCallback != null) {
+			// first reset this object's callback, then call it
+			Procedure callback = onActivationCallback;
+			onActivationCallback = null;
+			callback.execute();
+		} else {
+			logger.warn(String.format("Activating %s, but there is no activation callback registered", this));
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getId() {
-        // TODO correct?
-        return "SimulatedProcess" + getEntityId();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void passivate() {
+		if (onActivationCallback == null) {
+			logger.warn(String.format("Passivating %s, but there is no activation callback registered", this));
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ISchedulableProcess getRootProcess() {
-        // TODO: what is expected here? (copied this method body from SimuCom)
-        return null;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * Returns the request that created this simulated process.
-     */
-    public IRequest getRequest() {
-        return request;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ISchedulableProcess getRootProcess() {
+		// TODO: what is expected here? (copied this method body from SimuCom)
+		return null;
+	}
 
-    public SimulatedProcess getParent() {
-        return parent;
-    }
+	/**
+	 * Returns the request that created this simulated process.
+	 */
+	public IRequest getRequest() {
+		return request;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isFinished() {
-        return terminated;
-    }
+	public SimulatedProcess getParent() {
+		return parent;
+	}
 
-    /**
-     * Terminates this simulated process. Notifies all observers that has been registered by the
-     * {@code addTerminatedObserver} method.
-     * <p>
-     * If this simulated process terminated already, calling this method has no effect.
-     */
-    public void terminate() {
-        if (!terminated) {
-            terminated = true;
-            notifyLeftSystem();
-            fireTerminated();
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isFinished() {
+		return terminated;
+	}
 
-    public int getPriority() {
-        return priority;
-    }
+	/**
+	 * Terminates this simulated process. Notifies all observers that has been
+	 * registered by the {@code addTerminatedObserver} method.
+	 * <p>
+	 * If this simulated process terminated already, calling this method has no
+	 * effect.
+	 */
+	public void terminate() {
+		if (!terminated) {
+			terminated = true;
+			notifyLeftSystem();
+			fireTerminated();
+		}
+	}
 
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
+	public int getPriority() {
+		return priority;
+	}
 
-    /**
-     * Adds an observer which gets notifies when the process has ended its execution.
-     */
-    @Override
-    public void addTerminatedObserver(final IActiveResource o) {
-        terminatedObservers.add(o);
-    }
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
 
-    /**
-     * Notifies the observers which have been registered via
-     * {@link #addTerminatedObserver(IActiveResource)} that the process has ended it execution. This
-     * is automatically done, when {@link #terminate()} method is being invoked.
-     */
-    @Override
-    public void fireTerminated() {
-        for (IActiveResource o : terminatedObservers) {
-            o.notifyTerminated(this);
-        }
-        terminatedObservers.clear();
-    }
+	/**
+	 * Adds an observer which gets notifies when the process has ended its
+	 * execution.
+	 */
+	@Override
+	public void addTerminatedObserver(final IActiveResource o) {
+		terminatedObservers.add(o);
+	}
 
-    /**
-     * Removes the specified resource from the observer list.
-     */
-    @Override
-    public void removeTerminatedObserver(final IActiveResource o) {
-        terminatedObservers.remove(o);
-    }
+	/**
+	 * Notifies the observers which have been registered via
+	 * {@link #addTerminatedObserver(IActiveResource)} that the process has
+	 * ended it execution. This is automatically done, when {@link #terminate()}
+	 * method is being invoked.
+	 */
+	@Override
+	public void fireTerminated() {
+		for (IActiveResource o : terminatedObservers) {
+			o.notifyTerminated(this);
+		}
+		terminatedObservers.clear();
+	}
 
-    @Override
-    public void timeout(String timeoutFailureName) {
-        // TODO Failures are not yet supported
-        throw new RuntimeException("Encountered a timeout but simulation of failures is not supported.");
-    }
+	/**
+	 * Removes the specified resource from the observer list.
+	 */
+	@Override
+	public void removeTerminatedObserver(final IActiveResource o) {
+		terminatedObservers.remove(o);
+	}
 
-    @Override
-    public String getName() {
-        if (parent != null) {
-            return super.getName() + ", parent of " + parent.getName();
-        } else {
-            return super.getName();
-        }
-    }
+	@Override
+	public void timeout(String timeoutFailureName) {
+		// TODO Failures are not yet supported
+		throw new RuntimeException("Encountered a timeout but simulation of failures is not supported.");
+	}
 
-    public AbstractAction getCurrentPosition() {
-        return request.getCurrentPosition();
-    }
+	@Override
+	public String getName() {
+		if (parent != null) {
+			return super.getName() + ", parent of " + parent.getName();
+		} else {
+			return super.getName();
+		}
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = super.hashCode();
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        return result;
-    }
+	public AbstractAction getCurrentPosition() {
+		return request.getCurrentPosition();
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (!super.equals(obj))
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        SimulatedProcess other = (SimulatedProcess) obj;
-        if (getId() == null) {
-            if (other.getId() != null)
-                return false;
-        } else if (!getId().equals(other.getId()))
-            return false;
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		if (hashCode == 0) {
+			final int prime = 31;
+			int result = super.hashCode();
+			hashCode = prime * result + ((id == null) ? 0 : id.hashCode());
+		}
+		return hashCode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SimulatedProcess other = (SimulatedProcess) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }

d573798efedd297d0150a1f017fa122f6289c8ca
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-11-13 20:36:14 +0100

    PALLADIO-495 Attempt to adjust EventSim to recent changes in scheduler

--- bundles/edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/InjectableSchedulingFactory.java
+++ bundles/edu.kit.ipd.sdq.eventsim.resources/src/edu/kit/ipd/sdq/eventsim/resources/InjectableSchedulingFactory.java
@@ -6,13 +6,14 @@ import com.google.inject.Inject;
 
 import de.uka.ipd.sdq.scheduler.SchedulerModel;
 import de.uka.ipd.sdq.scheduler.factory.SchedulingFactory;
+import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
 
 @Singleton
 public class InjectableSchedulingFactory extends SchedulingFactory {
 
     @Inject
-    public InjectableSchedulingFactory(SchedulerModel model) {
-        super(model);
+    public InjectableSchedulingFactory(SchedulerModel model, IResourceTableManager resourceTableManager) {
+        super(model, resourceTableManager);
     }
 
 }
